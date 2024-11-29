c805bd56c4d51f9f7be99cdd63b678b77b5d01b2
JvDev16 <joaovictorfurtado021@gmail.com>
2024-11-28 22:01:13 -0300

    Primeiro commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View,Button, Alert } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+ 
+      <Text>Hello World!</Text> 
+      <Button title="Ir para Login" onPress={ () => Alert.alert("Estou funcionando") }/> 
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
